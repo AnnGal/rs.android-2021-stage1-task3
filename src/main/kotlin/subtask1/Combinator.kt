d2874f8998807e45c21db99896001d218041bb3d
Anya Galyamova <galyamova.anya@gmail.com>
2021-07-11 20:13:38 +0300

    add: task 1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,33 @@
 package subtask1
 
+/*fun main(args: Array<String>){
+    val res = Combinator().checkChooseFromArray(arrayOf(35, 7))
+    println(res)
+}*/
+
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val posters = array[0]
+        val colors = array[1]
+
+        for (colorNum in 1..colors) {
+            if (posters <= usedCombos(colors, colorNum)) {
+                return colorNum
+            }
+        }
+
+        return null
+    }
+
+    private fun usedCombos(colors: Int, colorNum: Int): Int {
+        return (factorial(colors) / (factorial(colorNum) * factorial(colors - colorNum))).toInt()
+    }
+
+    // factorial for number
+    private fun factorial(factorialNumber: Int): Long {
+        var result = 1L
+        if (factorialNumber >= 2) for (i in 2..factorialNumber) result *= i
+        return result
     }
 }
